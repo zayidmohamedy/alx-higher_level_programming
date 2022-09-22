e5cb9f9b3cd4d67f61a0da1ad183c58752c6f6f0
zayid <sayidka843@gmail.com>
2022-09-22 04:01:51 -0700

    updated Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == "__import__":
     import sys
     result = 0
     for arg in sys.argv:
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == "__import__":
     import hidden_4
-    for name in dir(hidden_4):
+    for name in dir(hidden_4.pyc):
         if name[0] != '_' and name[1] != '_':
             print(name)
